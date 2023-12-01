f094c414770feefde3431a3bc8451539951570f4
ReeZey <johannes@seved.net>
2023-12-01 18:28:41 +0100

    day one, part one

--- year-2023/day1/src/main.rs
+++ year-2023/day1/src/main.rs
@@ -1,3 +1,34 @@
+use std::{fs::File, io::Read};
+
 fn main() {
-    println!("Hello, world!");
+    let mut file_handle = File::open("sample.input").unwrap();
+
+    let mut sample_data = String::new();
+    file_handle.read_to_string(&mut sample_data).unwrap();
+
+    let mut total: u64 = 0;
+
+    for row in sample_data.split("\n") {
+        let mut first: Option<char> = None;
+        let mut last: Option<char> = None;
+
+        for char in row.chars() {
+            if char.is_numeric() {
+                if first.is_none() {
+                    first = Some(char);
+                }
+
+                last = Some(char);
+            }
+        }
+
+        if first.is_none() || last.is_none() {
+            continue;
+        }
+        let addative: u64 = format!("{}{}", first.unwrap(), last.unwrap()).parse().unwrap();
+
+        total += addative;
+    }
+
+    println!("{}", total);
 }
